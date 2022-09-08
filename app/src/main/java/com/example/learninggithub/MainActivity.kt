2eb93ce2ebec65f15ae723bf551d4ac0383dfc14
notaplayer4768 <thad.philpot@gmail.com>
2022-09-08 09:19:03 -0700

    added important variables y,z,x

--- app/src/main/java/com/example/learninggithub/MainActivity.kt
+++ app/src/main/java/com/example/learninggithub/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //important variables
+        val y = 20
+        val z = 30
+        val x = y+z
     }
 }
